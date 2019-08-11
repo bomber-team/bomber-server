2cf3ac1526cdbb69028306171689ff6004db240a
kostya05983 <kostya05983@mail.ru>
2019-08-11 22:42:22 +0700

    [ADD] early config server

---
+++ config/src/main/kotlin/bomber/Application.kt
@@ -0,0 +1,14 @@
+package bomber
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+
+@SpringBootApplication
+@EnableConfigServer
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
+
