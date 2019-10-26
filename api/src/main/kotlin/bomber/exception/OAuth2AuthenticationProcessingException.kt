625d27e06cf8a634388946b32451d36edb13a9b5
kostya05983 <kostya05983@mail.ru>
2019-10-26 21:21:03 +0700

    Delete old authorization

--- api/src/main/kotlin/bomber/exception/OAuth2AuthenticationProcessingException.kt
+++
@@ -1,9 +0,0 @@
-package bomber.exception
-
-import org.springframework.security.core.AuthenticationException
-
-class OAuth2AuthenticationProcessingException : AuthenticationException {
-    constructor(msg: String, t: Throwable) : super(msg, t) {}
-
-    constructor(msg: String) : super(msg) {}
-}
