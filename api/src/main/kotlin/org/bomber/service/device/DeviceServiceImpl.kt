eaf155c3d14eb27bb3799a460d779677674f47b1
kostya05983 <kostya05983@mail.ru>
2019-12-17 01:59:47 +0700

    Update configuration.

--- api/src/main/kotlin/org/bomber/service/device/DeviceServiceImpl.kt
+++ api/src/main/kotlin/org/bomber/service/device/DeviceServiceImpl.kt
@@ -9,22 +9,22 @@ import org.springframework.stereotype.Service
 @Service
 class DeviceServiceImpl : DeviceService {
     override suspend fun createDevice(createDeviceRequest: CreateDeviceRequest): DeviceDTO {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override suspend fun updateDevice(id: String, updateDeviceRequest: UpdateDeviceRequest): DeviceDTO {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override suspend fun getDevice(id: String): DeviceDTO {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override suspend fun getDevices(offset: Int, limit: Int): DeviceItemsDTO {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 
     override suspend fun deleteDevice(id: String): DeviceDTO {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
     }
 }
